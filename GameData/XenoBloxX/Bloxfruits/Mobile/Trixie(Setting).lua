5e14771b2ca0e45df0d83d988c5741572c95a633
TrixieXyz <xenobloxx@gmail.com>
2025-04-04 13:52:01 +0700

    Trixie(Setting).lua

--- GameData/XenoBloxX/Bloxfruits/Mobile/Trixie(Setting).lua
+++ GameData/XenoBloxX/Bloxfruits/Mobile/Trixie(Setting).lua
@@ -21,7 +21,7 @@ loadstring(game:HttpGet(("https://raw.githubusercontent.com/daucobonhi/Ui-Redz-V
   })
 
        MinimizeButton({
-       Image = "http://www.roblox.com/asset/?id=83190276951914",
+       Image = "https://www.roblox.com/asset/?id=83190276951914",
        Size = {40, 40},
        Color = Color3.fromRGB(20, 10, 30),
        Corner = true,
